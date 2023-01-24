f1cbd3932c19b5c6b1a2a1993d72f8af9e5fd9aa
Paul Hindt <phindt@gmail.com>
2023-01-23 22:49:18 -0800

    Fix build errors

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import { Post } from "../components/Post";
 import { posts } from "../getAllPosts";
 
 export default function IndexPage() {
+  if (!posts) return null;
   return (
     <>
       {posts.map((post) => (
