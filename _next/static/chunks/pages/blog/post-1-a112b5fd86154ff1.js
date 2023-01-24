3a4cf82111f73d468855ebcc0636c6c4e030d8b4
Paul Hindt <phindt@gmail.com>
2023-01-23 22:51:17 -0800

    Update static pages

---
+++ _next/static/chunks/pages/blog/post-1-a112b5fd86154ff1.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[383],{8168:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/blog/post-1",function(){return u(5833)}])}},function(n){n.O(0,[542,833,774,888,179],function(){return n(n.s=8168)}),_N_E=n.O()}]);
